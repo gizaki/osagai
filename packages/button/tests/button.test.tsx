2d1e329dd67fd0529f378385c0b917c3f7988308
eintxaurtieta <endikaintxaurtieta@gmail.com>
2021-02-09 21:07:28 +0100

    chore: good commit message

--- packages/button/tests/button.test.tsx
+++ packages/button/tests/button.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
-import { Button } from '../src/button'
+import { Button } from '../src'
 
 describe('Button', () => {
   test('renders Button component', () => {
-    render(<Button isFullWidth />)
+    render(<Button type="button" />)
 
     screen.debug()
   })
