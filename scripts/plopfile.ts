2d62952f75dedb9c6a82fb383c37d049bc44b9ec
eintxaurtieta <endikaintxaurtieta@gmail.com>
2021-02-11 00:00:22 +0100

    chore: add plop config to generate new components

---
+++ scripts/plopfile.ts
@@ -0,0 +1,57 @@
+import nodePlop, { ActionType } from 'node-plop'
+import shell from 'shelljs'
+import camelcase from 'camelcase'
+
+const plop = nodePlop('plop-templates/plopfile.hbs')
+
+interface Answers {
+  componentName: string
+  description: string
+  destinationDirectory: 'packages'
+}
+
+async function createPackage() {
+  plop.setHelper('capitalize', (text) => camelcase(text, { pascalCase: true }))
+
+  plop.setGenerator('component', {
+    description: 'Generates a component package',
+    prompts: [
+      {
+        type: 'input',
+        name: 'componentName',
+        message: 'Enter component name:',
+      },
+    ],
+
+    actions(answers: any) {
+      const actions: ActionType[] = []
+
+      if (!answers) return actions
+
+      const { componentName } = answers as Answers
+
+      actions.push({
+        type: 'addMany',
+        templateFiles: 'component/**',
+        destination: '../packages/{{dashCase componentName}}',
+        base: 'component/',
+        data: { componentName },
+        abortOnFail: true,
+      })
+
+      return actions
+    },
+  })
+
+  const { runPrompts, runActions } = plop.getGenerator('component')
+
+  const answers = await runPrompts()
+  await runActions(answers)
+}
+
+async function run() {
+  await createPackage()
+  shell.exec('yarn')
+}
+
+run()
